c0804441543e111d4ed8be49e53c0a11cfbedd22
Juan Duque <juuanduuke@gmail.com>
2025-03-20 17:47:04 -0500

    add interfaces for componente todo item

---
+++ src/app/features/todo/pages/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { TodoItem } from "../../../../core/domain/todo/todo-item.model";
+
+@Component({
+    selector: 'app-todo-item',
+    templateUrl: './todo-item.component.html',
+    standalone: true,
+    styleUrls: ['./todo-item.component.scss'],
+    imports: [CommonModule]
+
+})
+export class TodoItemComponent {
+    @Input() todo!: TodoItem;
+    @Output() delete = new EventEmitter<TodoItem>();
+    @Output() toggle = new EventEmitter<TodoItem>();
+
+    onDelete(): void {
+        this.delete.emit(this.todo);
+    }
+
+    onToggle(): void {
+        this.toggle.emit({ ...this.todo, completed: !this.todo.completed });
+    }
+}
