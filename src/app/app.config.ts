0ff8cb9706a9e301572b19b85416a3c583f18d4d
Juan Duque <juuanduuke@gmail.com>
2025-03-21 13:49:57 -0500

    Add enviroment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { TODO_REPOSITORY_TOKEN } from './core/domain/todo/todo.tokens';
 import { TodoMockService } from './core/infrastructure/todo/todo-mock.service';
+import { TodoHttpService } from './core/infrastructure/todo/todo-http.service';
+import { environment } from '../environment/environment';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    { provide: TODO_REPOSITORY_TOKEN, useClass: TodoMockService }
-
+    { provide: TODO_REPOSITORY_TOKEN, useClass: environment.useMock ? TodoMockService : TodoHttpService }
   ]
 };
--- src/app/core/infrastructure/todo/todo-http.service.ts
+++ src/app/core/infrastructure/todo/todo-http.service.ts
@@ -3,27 +3,31 @@ import { TodoRepository } from "../../domain/todo/todo.repository";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { TodoItem } from "../../domain/todo/todo-item.model";
+import { environment } from "../../../../environment/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TodoHttpService implements TodoRepository {
+
+    private readonly API_URL = environment.apiUrl;
+
     constructor(private http: HttpClient) {}
 
     create(todoItem: Partial<TodoItem>): Observable<TodoItem> {
-        return this.http.post<TodoItem>('/api/todos', todoItem);
+        return this.http.post<TodoItem>(this.API_URL, todoItem);
     }
 
     get(): Observable<TodoItem[]> {
-        return this.http.get<TodoItem[]>('/api/todos');
+        return this.http.get<TodoItem[]>(this.API_URL);
     }
 
     delete(id: number): Observable<any> {
-        return this.http.delete(`/api/todos/${id}`);
+        return this.http.delete(`${this.API_URL}/${id}`);
     }
     
     update(todo: TodoItem): Observable<TodoItem> {
-        return this.http.put<TodoItem>(`/api/todos/${todo.id}`, todo);
+        return this.http.put<TodoItem>(`${this.API_URL}/${todo.id}`, todo);
     }
       
 }
