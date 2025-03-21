ce6893899be3384461aabfab46eb0528d44d2dd6
Juan Duque <juuanduuke@gmail.com>
2025-03-21 11:56:11 -0500

    Update services [Mock y Http] and interface Repository also create UseCase and Test

---
+++ src/app/core/application/todo/delete-todo.usecase.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from "@angular/core";
+import { TODO_REPOSITORY_TOKEN } from "../../domain/todo/todo.tokens";
+import { TodoRepository } from "../../domain/todo/todo.repository";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DeleteTodoUseCase {
+
+    constructor(@Inject(TODO_REPOSITORY_TOKEN) private repo: TodoRepository) {}
+
+    execute(id: number): Observable<any> {
+        return this.repo.delete(id);
+    }
+}
