a05ba43cadcafba556bddca5e38109437576f929
Juan Duque <juuanduuke@gmail.com>
2025-03-21 22:18:06 -0500

    Add intercept and update providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,14 @@ import { TODO_REPOSITORY_TOKEN } from './core/domain/todo/todo.tokens';
 import { TodoMockService } from './core/infrastructure/todo/todo-mock.service';
 import { TodoHttpService } from './core/infrastructure/todo/todo-http.service';
 import { environment } from '../environment/environment';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { apiKeyInterceptor } from './core/infrastructure/interceptors/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    { provide: TODO_REPOSITORY_TOKEN, useClass: environment.useMock ? TodoMockService : TodoHttpService }
+    provideHttpClient(),
+    provideHttpClient(withInterceptors([apiKeyInterceptor])),
+    { provide: TODO_REPOSITORY_TOKEN, useClass: environment.useMock ? TodoMockService : TodoHttpService },
   ]
 };
---
+++ src/app/core/infrastructure/interceptors/api-key.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpRequest, HttpHandlerFn, HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environment/environment';
+
+export const apiKeyInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+  const clonedRequest = req.clone({
+    setHeaders: {
+      'x-api-key': environment.apiKey
+    }
+  });
+  return next(clonedRequest);
+};
