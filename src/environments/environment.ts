15119a5c87e63fc300568a30c82189d399f8ac7d
Juan Duque <juuanduuke@gmail.com>
2025-03-22 17:29:11 -0500

    fix error port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://vb73e5nvtc.us-east-1.awsapprunner.com:3000/api/todos',
+    apiUrl: 'https://vb73e5nvtc.us-east-1.awsapprunner.com/api/todos',
     useMock: false,
     apiKey: 'mi-secreta-api-key'
   };
